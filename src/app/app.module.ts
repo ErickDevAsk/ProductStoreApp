aa2f03050604449962cc7dd53175b9a58356a8ed
ErickDevAsk <112228747+ErickDevAsk@users.noreply.github.com>
2025-03-28 15:19:22 -0600

    primer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './views/product-details/product-details.component';
+import { CartComponent } from './views/cart/cart.component';
+import { AboutComponent } from './views/about/about.component';
+import { ProductsComponent } from './views/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule // <-- Agrega esto
   ],
   providers: [],
   bootstrap: [AppComponent]
